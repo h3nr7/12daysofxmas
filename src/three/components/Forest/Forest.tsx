32f5212da10135fb7818f227ebbe8ee925d9f900
Henry YP Ho <me@h3nr7.com>
2024-12-15 22:47:47 +0000

    added trees

--- src/three/components/Forest/Forest.tsx
+++ src/three/components/Forest/Forest.tsx
@@ -1,18 +1,28 @@
 import { CubeCamera, MeshRefractionMaterial, useGLTF } from '@react-three/drei'
 import { MeshProps, useLoader } from '@react-three/fiber'
 import { motion,  } from 'framer-motion-3d'
-import { useMemo, useRef } from 'react'
+import { Suspense, useMemo, useRef } from 'react'
 import { Mesh } from 'three'
 import { RGBELoader } from 'three/examples/jsm/Addons.js'
+import { useMobile } from '../../../hooks/isMobile'
 
 export default function Forest() {
   // const ref = useRef(null)
+
   const texture = useLoader(RGBELoader, 'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/aerodynamics_workshop_1k.hdr')
   const { nodes } = useGLTF('/tree.glb')
   const { nodes: fat_nodes } = useGLTF('/fat_tree.glb')
   const { nodes: trunk_nodes } = useGLTF('/trunk.glb')
   const { nodes: tiny_nodes } = useGLTF('/tiny_tree.glb')
-  console.log(tiny_nodes)
+
+  const isMobile = useMobile()
+
+  const { maxTree, maxFatTree, maxTinyTree, maxTrunk, distance } = useMemo(() => isMobile === true || isMobile === undefined ? {
+    maxTree: 50, maxFatTree: 50, maxTinyTree: 10, maxTrunk: 20, distance: 5
+  } : {
+    maxTree: 350, maxFatTree: 300, maxTinyTree: 150, maxTrunk: 150, distance: 30
+  }, [isMobile])
+
   const config = useMemo(() => {
 
     return {
@@ -24,63 +34,72 @@ export default function Forest() {
     }
   }, [])
 
-  const trees:Record<string, any>[] = []
-  const fatTrees:Record<string, any>[] = []
-  const tinyTrees:Record<string, any>[] = []
-  const trunks:Record<string, any>[] = []
+  const {trees, fatTrees, tinyTrees, trunks} = useMemo(() => {
+    const trees:Record<string, any>[] = []
+    const fatTrees:Record<string, any>[] = []
+    const tinyTrees:Record<string, any>[] = []
+    const trunks:Record<string, any>[] = []
 
-  for (let i=0; i<350; i++) {
-    trees.push({
-      x: -10 + Math.random() * 20,
-      y: 0,
-      z: -10 + Math.random() * 20,
-      rotateY: 2 * Math.PI * Math.random(),
-      scale: 0.05 + Math.random() * .2,
-      fresnel: Math.random() * 0.25,
-      ior: 1 + Math.random() * 0.1
-    })
-  }
+    for (let i=0; i<maxTree; i++) {
+      trees.push({
+        x: -distance/3 + Math.random() * distance*2/3,
+        y: 0,
+        z: -distance/3 + Math.random() * distance*2/3,
+        rotateY: 2 * Math.PI * Math.random(),
+        scale: 0.05 + Math.random() * .2,
+        fresnel: Math.random() * 0.25,
+        ior: 1 + Math.random() * 0.1
+      })
+    }
 
-  for (let i=0; i<300; i++) {
-    fatTrees.push({
-      x: -10 + Math.random() * 20,
-      y: 0,
-      z: -10 + Math.random() * 20,
-      rotateY: 2 * Math.PI * Math.random(),
-      scale: 0.1 + Math.random() * .2,
-      fresnel: Math.random() * 0.25,
-      ior: 1 + Math.random() * 0.1
-    })
-  }
+    for (let i=0; i<maxFatTree; i++) {
+      fatTrees.push({
+        x: -distance/3 + Math.random() * distance*2/3,
+        y: 0,
+        z: -distance/3 + Math.random() * distance*2/3,
+        rotateY: 2 * Math.PI * Math.random(),
+        scale: 0.1 + Math.random() * .2,
+        fresnel: Math.random() * 0.25,
+        ior: 1 + Math.random() * 0.1
+      })
+    }
 
-  for (let i=0; i<150; i++) {
-    tinyTrees.push({
-      x: -10 + Math.random() * 20,
-      y: 0,
-      z: -10 + Math.random() * 20,
-      rotateY: 2 * Math.PI * Math.random(),
-      scale: 0.05 + Math.random() * .2,
-      fresnel: Math.random() * 0.25,
-      ior: 1 + Math.random() * .1
-    })
-  }
+    for (let i=0; i<maxTinyTree; i++) {
+      tinyTrees.push({
+        x: -distance/3 + Math.random() * distance*2/3,
+        y: 0,
+        z: -distance/3 + Math.random() * distance*2/3,
+        rotateY: 2 * Math.PI * Math.random(),
+        scale: 0.05 + Math.random() * .2,
+        fresnel: Math.random() * 0.25,
+        ior: 1 + Math.random() * .1
+      })
+    }
 
-  for (let i=0; i<150; i++) {
-    trunks.push({
-      x: -10 + Math.random() * 20,
-      y: 0,
-      z: -10 + Math.random() * 20,
-      rotateY: 2 * Math.PI * Math.random(),
-      scale: 0.1 + Math.random() * .1
-    })
-  }
+    for (let i=0; i<maxTrunk; i++) {
+      trunks.push({
+        x: -distance/3 + Math.random() * distance*2/3,
+        y: 0,
+        z: -distance/3 + Math.random() * distance*2/3,
+        rotateY: 2 * Math.PI * Math.random(),
+        scale: 0.1 + Math.random() * .1
+      })
+    }
+
+    return {
+      trees,
+      fatTrees,
+      tinyTrees,
+      trunks
+    }
+  }, [isMobile])
 
   return (
     <CubeCamera resolution={256} frames={1} envMap={texture}>
       {(texture) => {
 
         return (
-          <>
+          <Suspense>
             {trees.map((t, i) => (
               <group key={i} >
                 <motion.mesh
@@ -142,7 +161,7 @@ export default function Forest() {
               {/* <MeshRefractionMaterial envMap={texture} {...config} toneMapped={false} /> */}
             </motion.mesh>
             ))}
-          </>
+          </Suspense>
         )
       }}
     </CubeCamera>

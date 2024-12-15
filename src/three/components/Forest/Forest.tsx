d00f65bbedf2d437cd00490b0ecd3a966dd12c5c
Henry YP Ho <me@h3nr7.com>
2024-12-15 22:15:49 +0000

    added mobile sass

--- src/three/components/Forest/Forest.tsx
+++ src/three/components/Forest/Forest.tsx
@@ -9,8 +9,10 @@ export default function Forest() {
   // const ref = useRef(null)
   const texture = useLoader(RGBELoader, 'https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/aerodynamics_workshop_1k.hdr')
   const { nodes } = useGLTF('/tree.glb')
+  const { nodes: fat_nodes } = useGLTF('/fat_tree.glb')
   const { nodes: trunk_nodes } = useGLTF('/trunk.glb')
-  console.log(nodes)
+  const { nodes: tiny_nodes } = useGLTF('/tiny_tree.glb')
+  console.log(tiny_nodes)
   const config = useMemo(() => {
 
     return {
@@ -23,23 +25,53 @@ export default function Forest() {
   }, [])
 
   const trees:Record<string, any>[] = []
+  const fatTrees:Record<string, any>[] = []
+  const tinyTrees:Record<string, any>[] = []
   const trunks:Record<string, any>[] = []
 
-  for (let i=0; i<500; i++) {
+  for (let i=0; i<350; i++) {
     trees.push({
       x: -10 + Math.random() * 20,
       y: 0,
       z: -10 + Math.random() * 20,
       rotateY: 2 * Math.PI * Math.random(),
-      scale: 0.1 + Math.random() * .2
+      scale: 0.05 + Math.random() * .2,
+      fresnel: Math.random() * 0.25,
+      ior: 1 + Math.random() * 0.1
     })
+  }
 
+  for (let i=0; i<300; i++) {
+    fatTrees.push({
+      x: -10 + Math.random() * 20,
+      y: 0,
+      z: -10 + Math.random() * 20,
+      rotateY: 2 * Math.PI * Math.random(),
+      scale: 0.1 + Math.random() * .2,
+      fresnel: Math.random() * 0.25,
+      ior: 1 + Math.random() * 0.1
+    })
+  }
+
+  for (let i=0; i<150; i++) {
+    tinyTrees.push({
+      x: -10 + Math.random() * 20,
+      y: 0,
+      z: -10 + Math.random() * 20,
+      rotateY: 2 * Math.PI * Math.random(),
+      scale: 0.05 + Math.random() * .2,
+      fresnel: Math.random() * 0.25,
+      ior: 1 + Math.random() * .1
+    })
+  }
+
+  for (let i=0; i<150; i++) {
     trunks.push({
       x: -10 + Math.random() * 20,
       y: 0,
       z: -10 + Math.random() * 20,
       rotateY: 2 * Math.PI * Math.random(),
-      scale: 0.1 + Math.random() * .2
+      scale: 0.1 + Math.random() * .1
     })
   }
 
@@ -50,12 +82,54 @@ export default function Forest() {
         return (
           <>
             {trees.map((t, i) => (
-              <motion.mesh key={i} 
-              initial={{ scale: 0, x: t.x, y: t.y, z: t.z, rotateY: t.rotateY }}
-              animate={{ scale: t.scale }}
-              geometry={(nodes.Cylinder001 as Mesh).geometry}>
-              <MeshRefractionMaterial envMap={texture} {...config} toneMapped={false} />
-            </motion.mesh>
+              <group key={i} >
+                <motion.mesh
+                  initial={{ scale: 0, x: t.x, y: t.y, z: t.z, rotateY: t.rotateY }}
+                  animate={{ scale: t.scale }}
+                  geometry={(nodes.Cylinder001 as Mesh).geometry}>
+                  <MeshRefractionMaterial envMap={texture} {...config} fresnel={t.fresnel} ior={t.ior} toneMapped={false} />
+                </motion.mesh>
+                <motion.mesh 
+                  initial={{ scale: 0}}
+                  animate={{ scale: t.scale }}
+                  position={[t.x, t.y, t.z]}
+                  material={(nodes.Cylinder001_1 as Mesh).material}
+                  geometry={(nodes.Cylinder001_1 as Mesh).geometry} />
+              </group>
+            ))}
+
+            {fatTrees.map((t, i) => (
+              <group key={i} >
+                <motion.mesh
+                  initial={{ scale: 0, x: t.x, y: t.y, z: t.z, rotateY: t.rotateY }}
+                  animate={{ scale: t.scale }}
+                  geometry={(fat_nodes.Cylinder as Mesh).geometry}>
+                  <MeshRefractionMaterial envMap={texture} {...config} fresnel={t.fresnel} ior={t.ior} toneMapped={false} />
+                </motion.mesh>
+                <motion.mesh 
+                  initial={{ scale: 0}}
+                  animate={{ scale: t.scale }}
+                  position={[t.x, t.y, t.z]}
+                  material={(fat_nodes.Cylinder_1 as Mesh).material}
+                  geometry={(fat_nodes.Cylinder_1 as Mesh).geometry} />
+              </group>
+            ))}
+
+            {tinyTrees.map((t, i) => (
+              <group key={i} >
+                <motion.mesh
+                  initial={{ scale: 0, x: t.x, y: t.y, z: t.z, rotateY: t.rotateY }}
+                  animate={{ scale: t.scale }}
+                  geometry={(tiny_nodes.Cylinder006 as Mesh).geometry}>
+                  <MeshRefractionMaterial envMap={texture} {...config} fresnel={t.fresnel} ior={t.ior} toneMapped={false} />
+                </motion.mesh>
+                <motion.mesh 
+                  initial={{ scale: 0}}
+                  animate={{ scale: t.scale }}
+                  position={[t.x, t.y, t.z]}
+                  material={(tiny_nodes.Cylinder006_1 as Mesh).material}
+                  geometry={(tiny_nodes.Cylinder006_1 as Mesh).geometry} />
+              </group>
             ))}
 
             {trunks.map((t, i) => (

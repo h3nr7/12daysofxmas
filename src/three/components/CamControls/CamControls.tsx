c88de8e31bc4f8582403d44c53acc0f0128de811
Henry YP Ho <me@h3nr7.com>
2024-12-28 19:37:16 +0000

    finally fixed the camera rig

--- src/three/components/CamControls/CamControls.tsx
+++ src/three/components/CamControls/CamControls.tsx
@@ -33,15 +33,15 @@ export function CamControls({
 }: ICamControl) {
   const { set: setThree, viewport }= useThree()
   const camRef = useRef<PerspectiveCamera>(null)
+  const controlRef = useRef<CameraControls>(null)
   const { data: { cam } } = useFiberStore()
-  const { setPosition, setRotationY, setRotationX } = useRigControl(ref, camRef)
-  const { drag } = useGestureControls()
+  const { setPosition } = useRigControl(ref, controlRef, camRef)
 
   useEffect(() => {
     if(!camRef.current) return
     camRef.current.aspect = viewport.width/viewport.height
     camRef.current.updateProjectionMatrix()
-  }, [])
+  }, [camRef.current])
 
   useEffect(() => {
     if(!camRef.current) return
@@ -49,45 +49,33 @@ export function CamControls({
   }, [camRef.current, isDefault])
 
   useEffect(() => {
-    console.log('cam look at changed', cam.lookAt)
+    controlRef.current?.setTarget(...cam.lookAt)
+  }, [controlRef.current])
+
+  useEffect(() => {
+    console.log(cam.lookAt)
     setPosition(cam.lookAt)
   }, [cam.lookAt])
-
-  // useEffect(() => {
-  //   if(!camRef.current || !ref.current?.rotateY) return
-  //   console.log('meme: ', drag)
-  //   setRotationY(drag[0] * Math.PI)
-  //   setRotationX(drag[1] * Math.PI)
-  // }, [drag])
 
 
   return (
     <motion.group ref={ref}>
-      {/* <mesh>
-        <sphereGeometry args={[1]} />
-        <Wireframe />
-      </mesh> */}
-      {/* <perspectiveCamera ref={camRef} position={[2, 2, 2]} >
-        <Helper type={CameraHelper} />
-      </perspectiveCamera> */}
-      {/* {debug && <CameraControls enabled={true} />} */}
-      <CameraControls />
+      <perspectiveCamera ref={camRef} position={[2.5, 4, 2.5]} >
+        {debug && <Helper type={CameraHelper} />}
+      </perspectiveCamera>
+      <CameraControls maxPolarAngle={Math.PI/2} ref={controlRef}/>
     </motion.group>
   )
 }
 
-function useRigControl(ref: RefObject<GroupProps>, camRef:RefObject<PerspectiveCamera>) {
+function useRigControl(ref: RefObject<GroupProps>, controlRef:RefObject<CameraControls>, camRef: RefObject<PerspectiveCamera>) {
 
   const [position, setPosition] = useState<Vec3>([0,0,0])
-  const [rotationY, setRotationY] = useState<number>(0)
-  const [rotationX, setRotationX] = useState<number>(0)
 
   const px = useSpring(position[0], DEFAULT_SPRING)
   const py = useSpring(position[1], DEFAULT_SPRING)
   const pz = useSpring(position[2], DEFAULT_SPRING)
 
-  const rotY = useSpring(rotationY)
-  const rotX = useSpring(rotationX)
 
   const pos = useTransform(() => new Vector3(px.get(), py.get(), pz.get()))
 
@@ -101,30 +89,11 @@ function useRigControl(ref: RefObject<GroupProps>, camRef:RefObject<PerspectiveC
 
   }, [position])
 
-  useEffect(() => { frame.read(() => rotY.set(rotationY)) }, [rotationY])
-  useEffect(() => { frame.read(() => rotX.set(rotationX)) }, [rotationX])
 
   useMotionValueEvent(pos, 'change', latest => {
-    if(!ref.current?.position) return
+    if(!ref.current?.position || !controlRef.current) return
     (ref.current.position as Vector3).set(latest.x, latest.y, latest.z)
-    ref.current.updateMatrix()
-    console.log('update latest: ', latest)
   })
 
-  useMotionValueEvent(rotY, 'change', latest => {
-    if(!ref.current?.rotateY) return
-    console.log('roty: ', latest)
-    ref.current.rotateY(latest)
-  })
-
-  useMotionValueEvent(rotX, 'change', latest => {
-    if(!camRef.current || !ref.current?.rotateZ) return
-    console.log('rotx: ', latest)
-    // ref.current.rotateX(latest)
-    camRef.current.rotateX(latest)
-  })
-
-
-
-  return { setPosition, setRotationY, setRotationX }
+  return { setPosition }
 }
